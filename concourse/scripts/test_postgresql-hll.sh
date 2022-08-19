d0ffb5f831f7aa816a5cbf204d7e9c0cba8e2453
liuxueyang <liuxueyang457@gmail.com>
2022-08-19 13:23:35 +0800

    Add pipeline (#3)
    - Add `-m` option to useradd
    - Optimize script file entry.sh, build_postgresql-hll.sh
    - Remove `gcs-bucket` variable passed to fly
    
    Co-authored-by: Chen Mulong <chenmulong@gmail.com>
    Co-authored-by: Xing Guo <admin@higuoxing.com>
    Co-authored-by: yihong <zouzou0208@gmail.com>

---
+++ concourse/scripts/test_postgresql-hll.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+
+set -exo pipefail
+
+function _main() {
+    unpack_dir=/home/gpadmin/bin_postgresql-hll/postgresql-hll_unpack
+    mkdir -p ${unpack_dir}
+    tar -xzf /home/gpadmin/bin_postgresql-hll/postgresql-hll-*_x86_64.tar.gz -C ${unpack_dir}
+    pushd ${unpack_dir}
+    ${unpack_dir}/install_gpdb_component
+    popd
+
+    source /home/gpadmin/gpdb_src/gpAux/gpdemo/gpdemo-env.sh
+
+    pushd /home/gpadmin/postgresql-hll_src
+    time make installcheck
+    popd
+}
+
+_main
